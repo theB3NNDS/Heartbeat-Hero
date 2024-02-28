da429d1cc8a8df75d796c3d74566083ca569f7ef
theB3NNDS <78061518+theB3NNDS@users.noreply.github.com>
2024-02-28 10:25:08 +0800

    feat: implemented title screen and splash screen
    others: added needed packages such as TMP and Recorder, added BGMs, added tile sprites and initialized tilemap

---
+++ Heartbeat Hero/Assets/Scripts/titlescreen.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class titlescreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene("stage1");
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
